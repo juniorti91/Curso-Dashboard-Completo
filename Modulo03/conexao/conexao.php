db1274433797f5e45e0093a319ceb2bc2e3e4f32
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2021-09-01 21:49:49 -0300

    Update conexao.php

--- Modulo03/conexao/conexao.php
+++ Modulo03/conexao/conexao.php
@@ -1,8 +1,8 @@
 <?php 
 
-$servername = "mysql873.umbler.com:41890";
-$username = "juniordash2";
-$password = "157486ba";
+$servername = "localhost";
+$username = "root";
+$password = "";
 $dbname = "junior_vendas";
 
 $conexao = mysqli_connect($servername, $username, $password, $dbname);
