a47ffcd86201673a23d33b00a08ead90e54e89cd
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2021-09-01 22:21:13 -0300

    01-09-2021 22:21

--- Modulo03/index.php
+++ Modulo03/index.php
@@ -1,43 +1,59 @@
-<?php 
+<?php
 
-    include 'conexao/conexao.php';
+include 'conexao/conexao.php';
 
-    $sql = "SELECT * FROM lucros";
-    $buscar = mysqli_query($conexao, $sql);
+$sql = "SELECT * FROM lucros";
+$buscar = mysqli_query($conexao, $sql);
 
-    #chart.js - Preparando valores#
+#chart.js - Preparando valores#
 
-    $mes = '';
-    $ano_2018 = '';
-    $ano_2019 = '';
-
-
-    while ($dados = mysqli_fetch_array($buscar)) {
-        
-        $mes = $mes . '"' . $dados['mes'] . '",';
-        $ano_2018 = $ano_2018 . '"' . $dados['ano_2018'] . '",';
-        $ano_2019 = $ano_2019 . '"' . $dados['ano_2019'] . '",';
+$mes = '';
+$ano_2018 = '';
+$ano_2019 = '';
 
 
-        $mes = trim($mes); #tira os espaços da variavel
-        $ano_2018 = trim($ano_2018);
-        $ano_2019 = trim($ano_2019);
-    }
+while ($dados = mysqli_fetch_array($buscar)) {
+
+    $mes = $mes . '"' . $dados['mes'] . '",';
+    $ano_2018 = $ano_2018 . '"' . $dados['ano_2018'] . '",';
+    $ano_2019 = $ano_2019 . '"' . $dados['ano_2019'] . '",';
+
+
+    $mes = trim($mes); #tira os espaços da variavel
+    $ano_2018 = trim($ano_2018);
+    $ano_2019 = trim($ano_2019);
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gráfico Linha</title>
 </head>
+
 <body>
 
     <script type="text/javascript">
-        
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+               labels: [<?php echo $mes ?>],
+               datasets: 
+               [{
+                   label: '2018',
+                   deta: [<?php echo $ano_2018 ?>],
+                   
+               }]
+            }
+            options: options
+        });
     </script>
-    
+
 </body>
+
 </html>
